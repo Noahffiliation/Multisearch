b63d8dbf83916c759b3716839cdac6a7301ff117
noahffiliation <nlindsey15@gmail.com>
2022-07-29 10:42:12 -0400

    Add song searching for Beat Saber and Clone Hero

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-document.getElementById("button").addEventListener("click", open_tabs);
+document.getElementById("mediaButton").addEventListener("click", open_media_tabs);
+document.getElementById("songButton").addEventListener("click", open_song_tabs);
 
-function open_tabs() {
-    let search_value = document.getElementById("searchBox").value;
+function open_media_tabs() {
+    let search_value = document.getElementById("mediaSearch").value;
     if (search_value != "") {
         window.open('https://letterboxd.com/search/'+search_value);
         window.open('https://trakt.tv/search?query='+search_value);
         window.open('https://myanimelist.net/search/all?q='+search_value);
         window.open('https://mydramalist.com/search?q='+search_value);
+    }
+}
+
+function open_song_tabs() {
+    let search_value = document.getElementById("songSearch").value;
+    if (search_value != "") {
+        window.open('https://chorus.fightthe.pw/search?query='+search_value);
+        window.open('https://bsaber.com/?s='+search_value)
     }
 }
